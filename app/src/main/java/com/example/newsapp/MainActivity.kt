f52e018328d4a703edb552325865b8a8de694460
Prateekthisside05 <83411276+Prateekthisside05@users.noreply.github.com>
2023-10-26 14:33:46 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.android.volley.Request
+import com.android.volley.toolbox.JsonObjectRequest
 
 class MainActivity : AppCompatActivity(), NewsItemClicked {
     private lateinit var mAdapter: NewsAdapter
@@ -18,13 +20,34 @@ class MainActivity : AppCompatActivity(), NewsItemClicked {
         recyclerView.adapter = mAdapter // Set the initialized mAdapter as the adapter for RecyclerView
         fetchData()
     }
-
+    //array list fetch karne ke liye function. index passing too
     private fun fetchData() {
-        // Your fetch data logic using Volley goes here
-        // Make sure to update the news data using mAdapter.updateNews(newsArray)
-    }
+        val url= "https://newsapi.org/v2/top-headlines?country=in&apiKey=383122df71e44cb982157cffc87c740f"
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                val newsJsonArray =it.getJSONArray("articles")
+                val newsArray = ArrayList<News>()
+                for (i in 0 until newsJsonArray.length()){
+                    val newsJsonObject = newsJsonArray.getJSONObject(i)
+                    val news = News(
+                        newsJsonObject.getString("title"),
+                        newsJsonObject.getString("author"),
+                        newsJsonObject.getString("url"),
+                        newsJsonObject.getString("urlToImage"),
+                    )
+                    newsArray.add(news)
+                }
+                mAdapter.updateNews(newsArray)
+            },
+            {
 
+            }
+        )
+        MySingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
+    }
     override fun onitemclicked(item: News) {
-        // Handle item click event
     }
 }
